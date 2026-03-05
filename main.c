bca10c5bd70bc2e8199131a6c92b8dc033ae0dc5
Frederico Melo <fredericomelo007@gmail.com>
2026-03-05 14:53:21 -0300

    comeco do codigo

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main() {
+    setlocale(LC_ALL,"Portuguese");
+    int opcao;
+
+    printf("== Menu ==  \n");
+    printf("1 - Cadastrar\n");
+    printf("2 - Listar\n");
+    printf("3 - Buscar por nome\n");
+    printf("4 - Editar\n");
+    printf("5 - Excluir\n");
+    printf("6 - Estatísticas\n");
+    printf("7 - Sair\n");
+    printf("Escolha uma opção: \n");
+    scanf("%d", &opcao)
+
+    switch (opcao){
+        
+    }
+
+        return 0;
+    }
